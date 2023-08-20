04059cb63f78c97568a61397ccb29b79423c25ee
IlmanNashran <ilmannashran@gmail.com>
2023-08-20 11:17:36 +0800

    create page history and bmi

---
+++ lib/pages/history_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HistoryPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HistoryPageState();
+  }
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        child: Container(
+      color: const Color.fromARGB(255, 142, 128, 128),
+    ));git
+  }
+}
