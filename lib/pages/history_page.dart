3285c77625b5ed6d93d062d28b924d9ade16845e
IlmanNashran <ilmannashran@gmail.com>
2023-08-20 20:57:21 +0800

    create history page widget

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_app/widgets/info_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,12 +10,54 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  double? _deviceHeight, _deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return CupertinoPageScaffold(
-      child: Container(
-        color: const Color.fromARGB(255, 142, 128, 128),
+      child: _dataCard(),
+    );
+  }
+
+  Widget _dataCard() {
+    return Center(
+      child: InfoCard(
+        height: _deviceHeight! * 0.25,
+        width: _deviceWidth! * 0.75,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _statusText("normal"),
+            _statusDate("05/3/2023"),
+            _statusbmi("27.4543"),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _statusText(String _status) {
+    return Text(
+      _status,
+      style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
+    );
+  }
+
+  Widget _statusDate(String _date) {
+    return Text(
+      _date,
+      style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+    );
+  }
+
+  Widget _statusbmi(String _bmi) {
+    return Text(
+      _bmi,
+      style: TextStyle(fontSize: 60, fontWeight: FontWeight.w600),
     );
   }
 }
